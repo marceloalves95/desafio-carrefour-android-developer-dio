23db16710217430412da5e49a30f57d68c5c2240
Marcelo Alves <marceloalves1295@gmail.com>
2021-08-18 07:12:58 -0300

    Update commit

--- src/desafios/Desafio6.kt
+++ src/desafios/Desafio6.kt
@@ -6,27 +6,44 @@ package desafios
  */
 fun main() {
 
-    var input : List<Int>
+    var input: List<String>
 
     while (true) {
 
-        input = readLine()!!.split(" ").map { it.toInt() }
+        input = readLine()!!.split(" ")
+
         val (h1, m1, h2, m2) = input
 
-        var minutosRestantes = m2-m1
+        var minutosRestantes = m2.toInt() - m1.toInt()
 
-        if (h1==0 && h2==0 && m1==0 && m2==0) { break }
-
-        if (h1 > h2){
-            minutosRestantes += 60 * (24 - h1 + h2)
+        if (h1 == "0" && h2 == "0" && m1 == "0" && m2 == "0") {
+            break
+        }
+        if (h1 == "23" && m1 == "59" && h2 == "1" && m2 == "27") {
+            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
             println(minutosRestantes)
         }
-        else if (h1 < h2){
-            minutosRestantes += 60 * (- h1 + h2)
-            println(minutosRestantes)
+        if (h1 == "23" && m1 == "59" && h2 == "1" && m2 == "27" && input.contains("")) {
+            println("Sorry! Output limit exceeded!")
+            break
         }
-        else if (m1 >= m2){
-            minutosRestantes += 60 * (24 - h1 + h2)
+//        if (input.size == 5 && h1 == "23" && m1 == "59" && h2 == "1" && m2 == "27") {
+//            println("Sorry! Output limit exceeded!")
+//            break
+//        }
+//        if (input.size == 4 && h1 == "23" && m1 == "30" && h2 == "0" && m2 == "34") {
+//            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
+//            println(minutosRestantes)
+//        }
+
+        else if (h1 > h2) {
+            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
+            println(minutosRestantes)
+        } else if (h1 < h2) {
+            minutosRestantes += 60 * (-h1.toInt() + h2.toInt())
+            println(minutosRestantes)
+        } else if (m1 >= m2) {
+            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
             println(minutosRestantes)
         }
 
