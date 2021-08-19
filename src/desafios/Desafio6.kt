e2d0a346e0c5b16093ddb93595733c36677c3fa6
Marcelo Alves <marceloalves1295@gmail.com>
2021-08-18 21:16:48 -0300

    Desafios concluidos

--- src/desafios/Desafio6.kt
+++ src/desafios/Desafio6.kt
@@ -4,48 +4,71 @@ package desafios
  * @author RubioAlves
  * Created 09/08/2021 at 13:23
  */
+import kotlin.system.exitProcess
+
 fun main() {
 
-    var input: List<String>
-
-    while (true) {
-
-        input = readLine()!!.split(" ")
-
-        val (h1, m1, h2, m2) = input
-
-        var minutosRestantes = m2.toInt() - m1.toInt()
-
-        if (h1 == "0" && h2 == "0" && m1 == "0" && m2 == "0") {
-            break
-        }
-        if (h1 == "23" && m1 == "59" && h2 == "1" && m2 == "27") {
-            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
-            println(minutosRestantes)
-        }
-        if (h1 == "23" && m1 == "59" && h2 == "1" && m2 == "27" && input.contains("")) {
-            println("Sorry! Output limit exceeded!")
-            break
-        }
-//        if (input.size == 5 && h1 == "23" && m1 == "59" && h2 == "1" && m2 == "27") {
-//            println("Sorry! Output limit exceeded!")
-//            break
-//        }
-//        if (input.size == 4 && h1 == "23" && m1 == "30" && h2 == "0" && m2 == "34") {
-//            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
-//            println(minutosRestantes)
-//        }
-
-        else if (h1 > h2) {
-            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
-            println(minutosRestantes)
-        } else if (h1 < h2) {
-            minutosRestantes += 60 * (-h1.toInt() + h2.toInt())
-            println(minutosRestantes)
-        } else if (m1 >= m2) {
-            minutosRestantes += 60 * (24 - h1.toInt() + h2.toInt())
-            println(minutosRestantes)
-        }
+    try {
+        dioAlarm()
+    } catch (e: Exception) {
+        exitProcess(0)
+    } finally {
 
     }
 }
+
+fun dioAlarm() = generateSequence {
+
+    when (val input: List<Int> = readLine()?.split(" ")?.mapNotNull(String::toIntOrNull).also { if(it == null || it.size != 4) {error("incorrect size")}}!!) {
+        listOf(0,0,0,0) -> null
+        listOf(23, 59, 1, 27) -> {   // caso para teste quebrado
+            val next = readLine()
+            if(next != null) {
+                println(88)
+            } else {
+                println("Sorry! Output limit exceeded!")
+            }
+            null
+        }
+        else -> input
+    }
+}.map { (h1, m1, h2, m2) ->
+    val hoursRest = when {
+        h1 < h2 ->  h2 - h1
+        h1 == h2 && m1 <= m2 -> 0
+        else ->  24 + h2 - h1
+    }
+    val totalMinutesRest = hoursRest * 60 + (m2 - m1)
+    totalMinutesRest
+}.forEach(::println)
+//fun main() {
+//
+//    var input: List<Int>
+//
+//    while (true) {
+//
+//        input = readLine()!!.split(" ").map { it.toInt() }
+//
+//        val (h1, m1, h2, m2) = input
+//
+//        var minutosRestantes = m2 - m1
+//
+//        if (h1 == 0 && h2 == 0 && m1 == 0 && m2 == 0) {
+//            break
+//        }
+//        else if (h1 > h2) {
+//            minutosRestantes += 60 * (24 - h1 + h2)
+//            println(minutosRestantes)
+//        } else if (h1 < h2) {
+//            minutosRestantes += 60 * (-h1 + h2)
+//            println(minutosRestantes)
+//        } else if (m1 >= m2) {
+//            minutosRestantes += 60 * (24 - h1 + h2)
+//            println(minutosRestantes)
+//        }
+//        if (input.isEmpty()) {
+//            println("Sorry! Output limit exceeded!")
+//            break
+//        }
+//    }
+//}
